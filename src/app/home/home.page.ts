33ec802d1fa2b795116c941fa9769d9ad34965ad
Niamh Kalusche <n.e.kalusche@gmail.com>
2026-04-17 18:07:43 +0100

    Testing Lifecycle Methods
    Adding lifecycle methods to homepage.ts, tesing them out with console logs and commenting accordingly

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,5 +7,36 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+  //testing constructor
+  constructor() {
+  console.log("constructor is working")
+
+  }
+
+  //testing ngoninit
+  ngOnInit(){
+    console.log("ngoninit is working")
+  }
+
+  //testing ionviewwillenter
+  ionViewWillEnter () {
+    console.log("ionviewWillEnter is working")
+  }
+  
+  //testing ionviewdidenter
+  ionViewDidEnter () {
+    console.log("ionViewDidEnter is working")
+  }
+
+  //testing ionviewwilleave
+  ionViewWillLeave () {
+    console.log("ionViewWillLeave is working")
+  }
+
+  //testing ionviewdidleaved
+  ionViewDidLeave () {
+    console.log("ionViewDidLeave is working")
+  }
 }
+
+
