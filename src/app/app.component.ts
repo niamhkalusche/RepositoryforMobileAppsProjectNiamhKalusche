eb871e85f9a063a8e16fe5f8fb5c004ac85ce0cc
Niamh Kalusche <n.e.kalusche@gmail.com>
2026-04-27 12:39:26 +0100

    Adding the togglemenu so the menu disappears when you click on a link and it opens a page rather than staying there even when the page is loaded. Created footer component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, RouterLink],
+  imports: [IonicModule, RouterLink ],
 })
 export class AppComponent {
   constructor() { }
