906d5c960ca62cec50e8032cb51cc685aa2d22c3
Niamh Kalusche <n.e.kalusche@gmail.com>
2026-05-01 19:54:07 +0100

    Added CSS to MovieDetails page for better display. Started to work on persondetails logic, so the user can click on an actor/crew member and view their details.

--- src/app/persondetails/persondetails.page.ts
+++ src/app/persondetails/persondetails.page.ts
@@ -1,20 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { MovieService } from '../services/movie';
+import { HttpOptions } from '@capacitor/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeaderComponent } from '../components/header/header.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-persondetails',
   templateUrl: './persondetails.page.html',
   styleUrls: ['./persondetails.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+    imports: [IonicModule, CommonModule, FormsModule, HeaderComponent, FooterComponent]
 })
-export class PersondetailsPage implements OnInit {
 
-  constructor() { }
+export class persondetailsPage implements OnInit {
 
-  ngOnInit() {
+  persondetails: any;
+
+  constructor(
+     private ms: MovieService,
+     private myActivatedRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit(){ 
+  this.movieId = this.myActivatedRoute.snapshot.paramMap.get('id');
+  this.getSearchedMovieDetails();
+   this.getMovieCredits();
   }
 
+  async getSearchedMovieDetails() {
+       const options: HttpOptions = {
+         url: 'https://api.themoviedb.org/3/movie/' + this.movieId,
+         headers: {
+           Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMmU3MzQwZTUzZDcxOWY3ZTBmNWZmNmIyMmViYmI2NiIsIm5iZiI6MTc3NzI5OTI2NC43MjYsInN1YiI6IjY5ZWY2ZjQwMDlkZWE4NDY1ZGQ0OTcxOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rmkBBt4VijAoQ-PXGUdr5FmRiuNswtSIK8XTcETi2Ro'
+         }
+       };
+   
+        const data = await this.ms.get(options);
+        this.moviedetails = data;  
+       console.log(this.moviedetails);
+     }
+
+     async getMovieCredits() {
+
+  const options: HttpOptions = {
+    url: 'https://api.themoviedb.org/3/movie/' + this.movieId + '/credits',
+    headers: {
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMmU3MzQwZTUzZDcxOWY3ZTBmNWZmNmIyMmViYmI2NiIsIm5iZiI6MTc3NzI5OTI2NC43MjYsInN1YiI6IjY5ZWY2ZjQwMDlkZWE4NDY1ZGQ0OTcxOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rmkBBt4VijAoQ-PXGUdr5FmRiuNswtSIK8XTcETi2Ro'
+    }
+  };
+
+  const data = await this.ms.get(options);
+
+  this.cast = data.cast;
+  this.crew = data.crew;
+
+  console.log(this.cast);
+  console.log(this.crew);
 }
+  }
+
